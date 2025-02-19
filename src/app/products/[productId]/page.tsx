e4088bf19abf92378042df92fe54ebfdac873efa
Surendar Yama <surendaryama@outlook.com>
2025-02-19 11:17:24 +0530

    nextjs tuts dynamic metadata done.

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,4 +1,17 @@
-export default async function ProductItemDetailPage({params}:{params:Promise<{productId:string}>}){
+import type {Metadata} from "next";
+
+type Props = {
+    params:Promise<{productId:string}>
+}
+
+export const generateMetadata = async ({params}: Props) : Promise<Metadata> => {
+    const {productId} = await params;
+    return {
+        title: `Product item ${productId}`,
+    }
+}
+
+export default async function ProductItemDetailPage({params}:Props){
 
     const {productId} = (await params);
     return (
